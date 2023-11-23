d998ee1a8aa304afd9819a6d150b20c5c07f6469
Peter van Hardenberg <pvh@pvh.ca>
2023-11-23 15:37:20 -0800

    first version of automerge-repo visibility plugin

---
+++ README.md
@@ -0,0 +1,16 @@
+# Automerge DevTools
+
+A Chrome DevTools extension to increase visibility into the state of your Automerge Repo.
+
+This extension abuses internal APIs and is likely to break without warning due to point releases.
+
+## Running this extension
+
+1. Clone this repository.
+2. Load this directory in Chrome as an [unpacked extension](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked).
+3. Navigate to a webpage and open the devtools window.
+4. Navigate to the new devtools panel named "demo panel", and click on the button.
+
+## Acknowledgements
+
+Initially authored by Peter van Hardenberg. Derived from [Chrome's sample code](https://github.com/GoogleChrome/chrome-extensions-samples/tree/main/api-samples/devtools/inspectedWindow).
---
+++ panel.js
@@ -0,0 +1,54 @@
+chrome.devtools.inspectedWindow.eval(
+  `
+  Object.values(window.repo.handles).map((handle) => {
+    const { url, state } = handle
+    let count = undefined
+    let heads = []
+    const doc = handle.docSync()
+    if (doc) {
+      count = Automerge.getAllChanges(doc).length
+      heads = Automerge.getHeads(doc)
+    }
+    return { url, state, count, heads }
+  })
+  `,
+  function (handles, isException) {
+    if (isException) {
+      document.body.innerText = "the page is not using automerge"
+      console.log(isException)
+      return
+    }
+    let table = document.createElement("table")
+    table.innerHTML = `
+      <thead>
+        <tr>
+          <th>URL</th>
+          <th>State</th>
+          <th>Num Changes</th>
+          <th>Heads</th>
+        </tr>
+      </thead>
+    `
+    table.style =
+      "width: 100%; border-collapse: collapse; border: 1px solid black; text-align: left; padding: 5px; margin: 5px;"
+    table.tHead.style =
+      "font-weight: bold; border-bottom: 1px solid black; padding: 5px; margin: 5px;"
+    handles.map(({ url, state, count, heads }) => {
+      let tr = document.createElement("tr")
+      let td = document.createElement("td")
+      td.innerText = url
+      tr.appendChild(td)
+      td = document.createElement("td")
+      td.innerText = state
+      tr.appendChild(td)
+      td = document.createElement("td")
+      td.innerText = count === undefined ? "?" : count
+      tr.appendChild(td)
+      td = document.createElement("td")
+      td.innerText = heads
+      tr.appendChild(td)
+      table.appendChild(tr)
+    })
+    document.body.appendChild(table)
+  }
+)
