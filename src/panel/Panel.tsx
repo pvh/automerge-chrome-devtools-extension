83e2f0c53d8461cafa95fa3e52ba3c8c43381092
Paul Sonnentag <paul.sonnentag@googlemail.com>
2024-07-22 14:03:50 +0200

    add tabs

--- src/panel/Panel.tsx
+++ src/panel/Panel.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import { DataTable } from "./DataTable";
 import { DocHandleInfo, columns } from "./schema";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export const Panel = () => {
   const [docHandlesInfo, setDocHandlesInfo] = useState<DocHandleInfo[]>([]);
+  const [selectedTab, setSelectedTab] = useState<string>();
 
   const refreshData = () => {
     getActiveHandlesInfo().then((info) => {
@@ -22,8 +24,29 @@ export const Panel = () => {
   }, []);
 
   return (
-    <div>
-      <DataTable columns={columns} data={docHandlesInfo} />
+    <div className="flex flex-col w-screen h-screen">
+      <div className="bg-gray-100">
+        <Tabs
+          defaultValue="documents"
+          className="w-fit"
+          onValueChange={setSelectedTab}
+          value={selectedTab}
+        >
+          <TabsList className="grid w-full grid-cols-2 rounded-none justify-start">
+            <TabsTrigger value="documents" className="w-fit">
+              Documents ({docHandlesInfo.length})
+            </TabsTrigger>
+            <TabsTrigger value="messages" className="w-fit">
+              Messages
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+      <div className="flex-1 min-h-0 overflow-auto">
+        {selectedTab === "documents" && (
+          <DataTable columns={columns} data={docHandlesInfo} />
+        )}
+      </div>
     </div>
   );
 };
