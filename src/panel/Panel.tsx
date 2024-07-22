a589b345e1afa517bdacfaff50175b03bc253892
Paul Sonnentag <paul.sonnentag@googlemail.com>
2024-07-22 11:33:33 +0200

    add nicer table

---
+++ src/panel/Panel.tsx
@@ -0,0 +1,46 @@
+import { DataTable } from "./Table";
+import { DocHandleInfo, columns } from "./schema";
+import { useState, useEffect } from "react";
+
+export const Panel = () => {
+  const [docHandlesInfo, setDocHandlesInfo] = useState<DocHandleInfo[]>([]);
+
+  useEffect(() => {
+    getActiveHandlesInfo().then((info) => {
+      setDocHandlesInfo(info);
+    });
+  }, []);
+
+  return (
+    <div>
+      <DataTable columns={columns} data={docHandlesInfo} />
+    </div>
+  );
+};
+
+const getActiveHandlesInfo = () =>
+  new Promise<DocHandleInfo[]>((resolve, reject) => {
+    chrome.devtools.inspectedWindow.eval(
+      `
+    Object.values(window.repo.handles).map((handle) => {
+      const { url, state } = handle
+      let count = undefined
+      let heads = []
+      const doc = handle.docSync()
+      if (doc) {
+        count = Automerge.getAllChanges(doc).length
+        heads = Automerge.getHeads(doc)
+      }
+      return { url, state, count, heads }
+    })
+    `,
+      (docHandlesInfo, isException) => {
+        if (isException) {
+          reject();
+          return;
+        }
+
+        resolve(docHandlesInfo as DocHandleInfo[]);
+      }
+    );
+  });
